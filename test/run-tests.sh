e6272ed4cd44481443708e89f56c5afa2b3a999c
Michał Stocki <mstocki@ydp.eu>
2016-12-06 14:42:33 +0100

    Test runner: Displaying diff of JSON output that don't match expected

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+PATH=$(npm bin):$PATH
 
 export DIR=$(pwd)
 
 for f in $( find . -name "test.sh" ); do
 	cd $(dirname $f)
 	chmod a+x ./test.sh
-	bash ./test.sh
+	bash ./test.sh > output.json
+	output=$(json-diff output.json expected-output.json);
+	if [ "$output" != " undefined" ] ; then
+	    echo "Test failed: $(pwd)"
+	    json-diff output.json expected-output.json
+	fi
+	rm output.json
 	cd "${DIR}"
 done
