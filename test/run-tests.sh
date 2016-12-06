763d4a794797f5d9934b1dbe03348ddc3ba1cd2e
Michał Stocki <mstocki@ydp.eu>
2016-12-06 14:57:33 +0100

    Test runner: returning exit code 1 in case of failing test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,6 +2,7 @@
 PATH=$(npm bin):$PATH
 
 export DIR=$(pwd)
+shouldFail=false
 
 for f in $( find . -name "test.sh" ); do
 	cd $(dirname $f)
@@ -9,9 +10,15 @@ for f in $( find . -name "test.sh" ); do
 	bash ./test.sh > output.json
 	output=$(json-diff output.json expected-output.json);
 	if [ "$output" != " undefined" ] ; then
+	    shouldFail=true
 	    echo "Test failed: $(pwd)"
 	    json-diff output.json expected-output.json
 	fi
 	rm output.json
 	cd "${DIR}"
 done
+
+if [ "$shouldFail" = true ] ; then
+    echo "Tests failed due to errors"
+    exit 1
+fi
